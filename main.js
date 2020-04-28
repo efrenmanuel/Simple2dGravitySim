d742a4f0ae9832b1201d4cd3a6b26053e441dbf7
Efrén Manuel González Diéguez <efrenmanuel@gmail.com>
2020-04-29 01:34:15 +0200

    Made the planet's limits change with the canvas size

--- main.js
+++ main.js
@@ -1,63 +1,49 @@
 
 p5.disableFriendlyErrors = true; // disables FES
 
-let width = 500;
-let height = 500;
+let width = 720;
+let height = 720;
 let planets;
-let character;
-
-let maxrotation = 2;
-
-let rotating = 0;
-let acceleratingrotation = 0;
-
-let decceleratingrotation = 0;
-
-let walking = 0;
-let strafing = 0;
-
-let fov = 90;
-let sliderFOV;
-
-let simpleRender;
-
-let detail = 50;
-let sliderDetail;
-
-let mouseLock = false;
 
 
 function setup() {
-    createCanvas(width, height );
+    createCanvas(width, height);
+    canvasSize=createVector(width,height);
     planets = [];
-    planets.push(new Planet(width/2, height/2,0,0, 25, 200, false, color(255,255,150)));
-    planets.push(new Planet(width/3*2, height/3,-40,-25, 20, 150, true, color(100,100,255)));
-    
-    planets.push(new Planet(width/3, height/5*4,20,-2, 20, 150, true, color(255,100,255)));
+    planets.push(new Planet(width / 2, height / 2, 0, 0, 25, 200, false, color(255, 255, 150),canvasSize));
+    planets.push(new Planet(width / 3 * 2, height / 3, -40, -25, 20, 150, true, color(100, 100, 255),canvasSize));
+
+    planets.push(new Planet(width / 3, height / 5 * 4, 20, -2, 20, 150, true, color(255, 100, 255),canvasSize));
     //planets.push(new Planet(0, 0, width, 0, color(255)));
     //planets.push(new Planet(width, height, 0, height, color(255)));
     //planets.push(new Planet(width, height, width, 0, color(255)));
     //for (let planet = 0; planet < 5; planet++) {
-        //planets.push(new Planet(random(width), random(height), random(width), random(height), color(random(100) + 125, random(100) + 125, random(100) + 125)));
-        //(planets[planets.length - 1]);
+    //planets.push(new Planet(random(width), random(height), random(width), random(height), color(random(100) + 125, random(100) + 125, random(100) + 125)));
+    //(planets[planets.length - 1]);
     //}
     createP("");
-    createButton("Add random planet").mouseClicked(randomPlanet); 
-    createButton("Add random star").mouseClicked(randomStar); 
+    createButton("Add random planet").mouseClicked(randomPlanet);
+    createButton("Add random star").mouseClicked(randomStar);
     frameRate(30);
 }
 
-function randomPlanet(){
-    size=random(10, 30)
-    weight=size*10;
-    planets.push(new Planet(random(width),random(height),random(-50,50),random(-50,50), size, weight, true,color(random(155,255),random(155,255),random(155,255))));
+function randomPlanet() {
+    size = random(10, 30);
+    weight = size * 10;
+    planets.push(new Planet(random(width), random(height), random(-50, 50), random(-50, 50), size, weight, true, color(random(155, 255), random(155, 255), random(155, 255),canvasSize)));
+}
+
+function randomPlanetWithPos(x, y) {
+    size = random(10, 30);
+    weight = size * 10;
+    planets.push(new Planet(x, y, random(-50, 50), random(-50, 50), size, weight, true, color(random(155, 255), random(155, 255), random(155, 255),canvasSize)));
 }
 
 
-function randomStar(){
-    size=random(20, 40)
-    weight=size*20;
-    planets.push(new Planet(random(width),random(height),random(-50,50),random(-50,50), size, weight, false,color(random(155,255),random(155,255),random(155,255))));
+function randomStar() {
+    size = random(20, 40);
+    weight = size * 20;
+    planets.push(new Planet(random(width), random(height), random(-50, 50), random(-50, 50), size, weight, false, color(random(155, 255), random(155, 255), random(155, 255),canvasSize)));
 }
 
 function draw() {
@@ -67,7 +53,7 @@ function draw() {
         planet.updatePos(planets);
         planet.show();
     }
-  
+
 }
 
 function keyTyped() {
@@ -84,27 +70,20 @@ function mouseDragged() {
     return;
     if (mouseX < width && mouseX > 0) {
         if (mouseY < height && mouseY > 0) {
-            character.pos = createVector(mouseX, mouseY)
+            character.pos = createVector(mouseX, mouseY);
         }
 
     }
 }
 function mouseClicked() {
-    return;
-    
+
     if (mouseX < width && mouseX > 0) {
         if (mouseY < height && mouseY > 0) {
-            character.pos = createVector(mouseX, mouseY);
+            randomPlanetWithPos(mouseX, mouseY);
         }
-        else if (mouseY < height * 2 && mouseY > 0) {
-            if (!mouseLock){
-            requestPointerLock();
-            mouseLock = true;}else{
-                exitPointerLock();
-                mouseLock=false;
-            }
-        }
-    }
+    
+
+}
 
 }
 
@@ -117,7 +96,7 @@ function keyPressed() {
         if (key === "d") {
             strafing = 1;
         }
-       
+
     }
     else {
         if (key === "a") {
@@ -162,4 +141,4 @@ function keyReleased() {
         }
     }
 
-}
+};
--- planet.js
+++ planet.js
@@ -1,7 +1,7 @@
 class Planet{
-    
     GravForce=6.67*Math.pow(10,-11);
-    constructor(x, y, vx, vy, radius, mass, move, pColor){
+    
+    constructor(x, y, vx, vy, radius, mass, move, pColor, canvasSize){
         this.position=createVector(x, y);
         this.radius=radius;
         this.pColor=pColor;
@@ -9,6 +9,7 @@ class Planet{
         this.velocityY=vy;
         this.mass=mass;
         this.move=move;
+        this.canvasSize=canvasSize;
     }
 
 
@@ -17,7 +18,7 @@ class Planet{
         
         fill(this.pColor);
         push();
-        circle(this.position.x, this.position.y,this.radius*2)
+        circle(this.position.x, this.position.y,this.radius*2);
         pop();
     }
     updatePos(bodies){
@@ -35,13 +36,13 @@ class Planet{
             }
         let time=1/30;
         this.position.x+=time*this.velocityX;
-        if (this.position.x<0 || this.position.x>500){
+        if (this.position.x<0 || this.position.x>canvasSize.x){
             this.velocityX=-this.velocityX;
             this.position.x+=time*this.velocityX*2;
         }
         this.position.y+=time*this.velocityY;
         
-        if (this.position.y<0 || this.position.y>500){
+        if (this.position.y<0 || this.position.y>canvasSize.y){
             this.velocityY=-this.velocityY;
             this.position.y+=time*this.velocityY*2;
         }
@@ -59,4 +60,4 @@ class Planet{
         return acceleration*Math.pow(10,14);
     }
    
-}
+};
